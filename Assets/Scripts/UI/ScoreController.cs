2f793ba8862946a1fcea17740578b9c457137c86
microjumper <microjmpr@gmail.com>
2025-08-19 19:34:15 +0200

    Add score

---
+++ Assets/Scripts/UI/ScoreController.cs
@@ -0,0 +1,57 @@
+using System;
+using Playfield;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UI
+{
+    public class ScoreController : MonoBehaviour
+    {
+        private Label leftScoreLabel;
+        private Label rightScoreLabel;
+        
+        private int leftScore = 0;
+        private int rightScore = 0;
+    
+        private void Awake()
+        {
+            GetReferences();
+        }
+    
+        private void OnEnable()
+        {
+            GoalLine.OnGoalScored += HandleGoalScored;
+        }
+
+        private void OnDisable()
+        {
+            GoalLine.OnGoalScored -= HandleGoalScored;
+        }
+
+        private void GetReferences()
+        {
+            var root = GetComponent<UIDocument>().rootVisualElement;
+            var labelContainer = root.Q<VisualElement>("label__container");
+
+            leftScoreLabel = labelContainer.Q<Label>("left-score__label");
+            rightScoreLabel = labelContainer.Q<Label>("right-score__label");
+        }
+
+        private void HandleGoalScored(Side side)
+        {
+            switch (side)
+            {
+                case Side.Left:
+                    rightScore++;
+                    rightScoreLabel.text = rightScore.ToString();
+                    break;
+                case Side.Right:
+                    leftScore++;
+                    leftScoreLabel.text = leftScore.ToString();   
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(side), side, null);
+            }
+        }
+    }
+}
